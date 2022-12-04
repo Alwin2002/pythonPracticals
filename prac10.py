eb836a92477335fed72b24cb0f1c5b2114e8c211
Alwin2002 <alwinantu0909@gmail.com>
2022-12-04 15:04:57 +0530

    commit it

--- prac10.py
+++ prac10.py
@@ -1,8 +1,12 @@
-def fac(n):
-    if n==0:
-        return 1
-    else:
-        return n*fac(n-1)
-a=int(input("Enter a number : "))
-print(fac(a))
+def fibo(n):
+  if n == 1:
+    return 0
+  elif n == 2:
+    return 1
+  else:
+  return (fibo(n - 1) + (n - 2))
+n = int(input("Enter number of terms : "))
+for i in range(1, n + 1):
+print(fibo(i))
 print(" ")
+
