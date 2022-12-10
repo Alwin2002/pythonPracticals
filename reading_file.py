ae8dad379dcd76ab35c87fe13bc4c6b90c47d81c
alwin <alwinantu0909@gmail.com>
2022-12-10 21:00:59 +0530

    commit

--- reading_file.py
+++ reading_file.py
@@ -1,3 +1,13 @@
-f = open('testing.txt') 
-data = f.read()
-print(data)
+data = open('testing.txt').read()
+
+print('Reading the whole file :',data)
+
+#reading data with btyes defined 
+
+data1 = open('testing.txt').read(3)
+print('Reading only three 3 bytes: ',data1)
+
+#reading data with readline method
+
+data2=open('testing.txt').readlines()
+print('Reading lines with readline method:',data2)
